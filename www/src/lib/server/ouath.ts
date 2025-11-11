a66175f43045cab7bbbb078c26374df748bcc7d4
sam <woodstocksamuel7@gmail.com>
2025-11-10 22:58:35 -0500

    make callback_url

--- www/src/lib/server/ouath.ts
+++ www/src/lib/server/ouath.ts
@@ -1,8 +1,13 @@
 import { Google } from "arctic";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, HOST, PORT, SCHEME } from "$env/static/private";
-
+let callback_url = ""
+if (PORT) {
+	callback_url = `${SCHEME}://${HOST}:${PORT}/google/callback`
+} else {
+	callback_url = `${SCHEME}://${HOST}/google/callback`
+}
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
-	`${SCHEME}://${HOST}:${PORT}/google/callback`
+	callback_url
 );
